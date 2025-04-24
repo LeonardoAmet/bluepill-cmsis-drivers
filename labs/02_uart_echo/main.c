453ae8b1218199f194f14d4bb24d21720fe9a255
Leonardo Amet <leonardo.amet@gmail.com>
2025-04-24 19:45:47 +0000

    Configuración estable con HSI/2 * 9 = 36 MHz y USART funcional

--- labs/02_uart_echo/main.c
+++ labs/02_uart_echo/main.c
@@ -1,14 +1,17 @@
-#define STM32F103xB
+#include "stm32f1xx.h"
 #include "usart.h"
 
 int main(void) {
+    SystemCoreClockUpdate();  // ¡Refresca el valor con seguridad!
+
     usart_init(USART1, 9600);
     usart_write_string(USART1, "UART Echo Ready\r\n");
-    while (1) {
-        char c = usart_read_char(USART1);
-        usart_write_char(USART1, c);
-        usart_write_char(USART1, 'A');
 
+    while (1) {
+        char c = 'A';
+        usart_write_char(USART1, c);
+
+        for (volatile int i = 0; i < 50000; i++); // fix del loop también
     }
 }
 
